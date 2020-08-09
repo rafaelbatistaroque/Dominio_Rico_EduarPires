605766d43c8b925ac56dd795bedecff9e798a91e
Rafael Batista <maranata_rafa@hotmail.com>
2020-08-09 15:34:22 -0400

    Primeiro commit; estrutura inicial para projeto e; criado entidade produto e categoria.

---
+++ NerdStore/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity: IEquatable<Entity>
+    {
+        public Guid Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals([AllowNull] Entity other)
+        {
+            return Id == other.Id;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
